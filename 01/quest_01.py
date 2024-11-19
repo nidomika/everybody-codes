c44fa39c0c400d90e76be9ee41fd120543aca8bc
Dominika Szymanska <dominika1872@gmail.com>
2024-11-19 14:12:40 +0100

    Update quest_01 to use helper functions

--- 01/quest_01.py
+++ 01/quest_01.py
@@ -1,24 +1,29 @@
 from functions import read_input, split_list
 
+
 def part1(input_data) -> int:
-    return sum(0 if x == 'A' else 1 if x == 'B' else 3 for x in input_data)
+    return sum(0 if x == 'A' else 1 if x == 'B' else 3 for x in input_data[0])
 
 
 def part2(input_data) -> int:
-    input_data = [x.replace('x', '') for x in list(split_list(input_data, 2))]
+    input_data = [x.replace('x', '')
+                  for x in list(split_list(input_data[0], 2))]
     counter = 0
     for pair in input_data:
-        counter += sum(0 if x == 'A' else 1 if x == 'B' else 3 if x == 'C' else 5 for x in pair)
+        counter += sum(0 if x == 'A' else 1 if x ==
+                       'B' else 3 if x == 'C' else 5 for x in pair)
         if len(pair) == 2:
             counter += 2
     return counter
 
 
 def part3(input_data) -> int:
-    input_data = [x.replace('x', '') for x in list(split_list(input_data, 3))]
+    input_data = [x.replace('x', '')
+                  for x in list(split_list(input_data[0], 3))]
     counter = 0
     for pair in input_data:
-        counter += sum(0 if x == 'A' else 1 if x == 'B' else 3 if x == 'C' else 5 for x in pair)
+        counter += sum(0 if x == 'A' else 1 if x ==
+                       'B' else 3 if x == 'C' else 5 for x in pair)
         if len(pair) == 2:
             counter += 2
         elif len(pair) == 3:
